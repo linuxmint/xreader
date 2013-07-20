1b06b47447b7e451ac7f2078787cbb190e32ff30
Carlos Garcia Campos <carlosgc@gnome.org>
2013-07-20 11:05:12 +0200

    EVINCE BACKPORTS: libview: Fix selections starting/ending in page margins
    Backported by Peter Moser <pitiz29a@gmail.com>
    
    Taken from:
    https://gitlab.gnome.org/GNOME/evince/commit/4969e2b29d32f8177988a4bf4f62d7704f39f5e1
    
    commit 4969e2b29d32f8177988a4bf4f62d7704f39f5e1
    Author: Carlos Garcia Campos <carlosgc@gnome.org>
    Date:   Sat Jul 20 11:05:12 2013 +0200
    
        libview: Fix selections starting/ending in page margins
    
        Do not take into account the page area borders when looking the pages
        where the selection starts or ends. This allows to start a new selection
        from a page margin and prevents selection from disappearing when
        selecting multiple pages and releasing the mouse between two pages.

--- libview/ev-view.c
+++ libview/ev-view.c
@@ -6439,6 +6439,10 @@ compute_new_selection_text (EvView          *view,
 		GtkBorder border;
 
 		ev_view_get_page_extents (view, i, &page_area, &border);
+		page_area.x -= border.left;
+		page_area.y -= border.top;
+		page_area.width += border.left + border.right;
+		page_area.height += border.top + border.bottom;
 		if (gdk_rectangle_point_in (&page_area, start) ||
 		    gdk_rectangle_point_in (&page_area, stop)) {
 			if (first == n_pages)
@@ -6466,6 +6470,10 @@ compute_new_selection_text (EvView          *view,
 		selection->rect.y2 = height;
 
 		ev_view_get_page_extents (view, i, &page_area, &border);
+		page_area.x -= border.left;
+		page_area.y -= border.top;
+		page_area.width += border.left + border.right;
+		page_area.height += border.top + border.bottom;
 
 		if (gdk_rectangle_point_in (&page_area, start))
 			point = start;
