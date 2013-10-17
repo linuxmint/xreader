764d5de8530a2884e249b3277428d8466358dd05
raveit65 <chat-to-me@raveit.de>
2013-10-17 23:49:36 +0200

    rename to atril

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="mate-document-viewer"
+PKG_NAME="atril"
 
 (test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
