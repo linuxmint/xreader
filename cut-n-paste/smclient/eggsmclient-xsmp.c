a25645171c0986c74b1e4df2146782a69ebb81b6
Stefano Karapetsas <stefano@karapetsas.com>
2014-02-24 15:40:54 +0100

    Add missing include

--- cut-n-paste/smclient/eggsmclient-xsmp.c
+++ cut-n-paste/smclient/eggsmclient-xsmp.c
@@ -37,6 +37,7 @@
 
 #include <gdk/gdk.h>
 #include <gtk/gtk.h>
+#include <gdk/gdkx.h>
 
 #define EGG_TYPE_SM_CLIENT_XSMP            (egg_sm_client_xsmp_get_type ())
 #define EGG_SM_CLIENT_XSMP(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), EGG_TYPE_SM_CLIENT_XSMP, EggSMClientXSMP))
