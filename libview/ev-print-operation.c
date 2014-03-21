9d41869e2e499aec7fc305cade177c87b9a1ea8d
Marek Kasik <mkasik@redhat.com>
2014-03-21 14:10:26 +0100

    EVINCE BACKPORTS: libview: Show correct warning when printing
    Backported by Peter Moser <pitiz29a@gmail.com>
    
    Taken from:
    https://gitlab.gnome.org/GNOME/evince/commit/33a4d5d4e35710c832bb
    
    commit 33a4d5d4e35710c832bbdee736a0219ca86f63fe
    Author: Marek Kasik <mkasik@redhat.com>
    Date:   Fri Mar 21 14:10:26 2014 +0100
    
        libview: Show correct warning when printing
    
        Show that requested format is not supported by printer
        if it is not supported.
    
        https://bugzilla.gnome.org/show_bug.cgi?id=724044

--- libview/ev-print-operation.c
+++ libview/ev-print-operation.c
@@ -1159,7 +1159,7 @@ ev_print_operation_export_print_dialog_response_cb (GtkDialog              *dial
 		g_set_error_literal (&export->error,
                                      GTK_PRINT_ERROR,
                                      GTK_PRINT_ERROR_GENERAL,
-                                     _("Printing is not supported on this printer."));
+                                     _("Requested format is not supported by this printer."));
 		g_signal_emit (op, signals[DONE], 0, GTK_PRINT_OPERATION_RESULT_ERROR);
 		
 		return;
