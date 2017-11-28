95b16f4a22ee8fcd9d8bd6ae14b968a3d1f88454
Mickael Albertus <mickael.albertus@gmail.com>
2017-11-29 00:59:04 +0100

    Change Icon to FlowBox

--- shell/ev-recent-view.c
+++ shell/ev-recent-view.c
@@ -29,10 +29,9 @@
 #include "ev-job-scheduler.h"
 
 struct _EvRecentViewPrivate {
-        GtkIconView      *view;
-        GtkListStore     *model;
-        GtkTreePath      *pressed_item_tree_path;
-        guint             recent_manager_changed_handler_id;
+        GtkFlowBox         *view;
+        GtkRecentManager   *recent_manager;
+        guint               recent_manager_changed_handler_id;
 };
 
 typedef enum {
@@ -52,14 +51,6 @@ static guint signals[NUM_SIGNALS] = { 0, };
 G_DEFINE_TYPE (EvRecentView, ev_recent_view, GTK_TYPE_SCROLLED_WINDOW)
 
 #define THUMBNAIL_WIDTH 80
-#define MAX_RECENT_VIEW_ITEMS 20
-
-static void
-ev_recent_view_clear_model (EvRecentView *ev_recent_view)
-{
-        EvRecentViewPrivate *priv = ev_recent_view->priv;
-        gtk_list_store_clear (priv->model);
-}
 
 static gint
 compare_recent_items (GtkRecentInfo *a,
@@ -87,55 +78,53 @@ compare_recent_items (GtkRecentInfo *a,
         return 0;
 }
 
-static void
-thumbnail_job_completed_callback (EvJobThumbnail *job,
-				  				  EvRecentView *ev_recent_view)
+static gchar *
+format_name (const gchar *name)
 {
-	GtkTreeIter 		*iter = (GtkTreeIter *) g_object_get_data (G_OBJECT (job), "tree_iter");
-	EvRecentViewPrivate *priv = ev_recent_view->priv;
+	GString *str = g_string_new (name);
+	guint length = strlen (name);
 	
-	gtk_list_store_set (priv->model,
-			    		iter,
-			    		0, job->thumbnail,
-			    		-1);
+	if (length <= 32)
+		return name;
+	
+	g_string_erase (str, 20, length-32);
+	g_string_insert (str, 20, "...");
+	return g_string_free (str, FALSE);
 }
 
 static void
-on_icon_view_item_activated (GtkIconView  *iconview,
-                             GtkTreePath  *path,
-                             EvRecentView *ev_recent_view)
+thumbnail_job_completed_callback (EvJobThumbnail *job,
+				  				  GtkButton *button)
 {
-        EvRecentViewPrivate *priv = ev_recent_view->priv;
-        GtkTreeIter          iter;
-        gchar               *uri;
+	if (!ev_job_is_failed (EV_JOB (job))) {
+		gtk_button_set_image (button,
+							  gtk_image_new_from_pixbuf(job->thumbnail));
+	}
+}
 
-        if (!gtk_tree_model_get_iter (GTK_TREE_MODEL (priv->model), &iter, path))
-                return;
-
-        gtk_tree_model_get (GTK_TREE_MODEL (priv->model), &iter,
-                            EV_RECENT_VIEW_COLUMN_URI, &uri,
-                            -1);
-        g_signal_emit (ev_recent_view, signals[ITEM_ACTIVATED], 0, uri);
-        g_free (uri);
+static void
+on_item_activated (GtkButton *button,
+               	   EvRecentView    *ev_recent_view)
+{
+	gchar *uri = (gchar *) g_object_get_data (G_OBJECT (button), "uri");
+    g_signal_emit (ev_recent_view, signals[ITEM_ACTIVATED], 0, uri);
+    g_free (uri);
 }
 
 static void
 ev_recent_view_refresh (EvRecentView *ev_recent_view)
 {
 	GList *items, *l;
-	GtkRecentManager 	*recent_manager = gtk_recent_manager_get_default ();
 	const gchar 	 	*xreader = g_get_application_name ();
 	EvRecentViewPrivate *priv = ev_recent_view->priv;
-
 	
-	items = gtk_recent_manager_get_items (recent_manager);
+	items = gtk_recent_manager_get_items (priv->recent_manager);
 	items = g_list_sort (items, (GCompareFunc) compare_recent_items);
 	
 	for (l = items; l && l->data; l = g_list_next (l)) {
-		GtkRecentInfo            *info;
-		const gchar              *name, *uri;
-		GdkPixbuf                *pixbuf;
-		GtkTreeIter               iter;
+		GtkRecentInfo  *info;
+		const gchar    *name, *uri;
+		GdkPixbuf      *pixbuf;
 		
 		info = (GtkRecentInfo *) l->data;
 
@@ -145,22 +134,30 @@ ev_recent_view_refresh (EvRecentView *ev_recent_view)
 		if (gtk_recent_info_is_local (info) && !gtk_recent_info_exists (info))
 		        continue;
 
-		name = gtk_recent_info_get_display_name (info);
+		name = format_name (gtk_recent_info_get_display_name (info));
 		uri = gtk_recent_info_get_uri (info);
 		pixbuf = gtk_recent_info_get_icon (info, THUMBNAIL_WIDTH);
-		gtk_list_store_append (priv->model, &iter);
-		gtk_list_store_set (priv->model, &iter, 
-							EV_RECENT_VIEW_COLUMN_PIXBUF, pixbuf, 
-							EV_RECENT_VIEW_COLUMN_NAME, name, 
-							EV_RECENT_VIEW_COLUMN_URI, uri,
-							-1);
 		
+		GtkButton *button = gtk_button_new ();
+		
+		gtk_button_set_label (button, name);
+		gtk_button_set_always_show_image (button, TRUE);
+		gtk_button_set_image (button, gtk_image_new_from_pixbuf (pixbuf));
+		gtk_button_set_image_position (button, GTK_POS_TOP);
+		gtk_button_set_relief (button, GTK_RELIEF_NONE);
+		
+		gtk_container_add (priv->view, button);
+		gtk_widget_show (button);
+		
+		g_object_set_data (G_OBJECT (button), "uri", uri);
+		g_signal_connect (button, "clicked",
+		                      G_CALLBACK (on_item_activated),
+		                      ev_recent_view);
 		
 		EvDocument *document = ev_document_factory_get_document (uri, NULL);
 		if (document) {
 			gdouble width;
 			if (document->iswebdocument == TRUE ) {
-				/* Hardcoded for epub documents*/
 				width = 800;
 			} else {
 				ev_document_get_page_size (document, 0, &width, NULL);
@@ -168,16 +165,11 @@ ev_recent_view_refresh (EvRecentView *ev_recent_view)
 			
 			width = (gdouble)THUMBNAIL_WIDTH / width;
 			EvJobThumbnail *job = ev_job_thumbnail_new (document, 0, 0, width);
-			g_object_set_data_full (G_OBJECT (job), "tree_iter",
-						gtk_tree_iter_copy (&iter),
-						(GDestroyNotify) gtk_tree_iter_free);
 			g_signal_connect (job, "finished",
-					  G_CALLBACK (thumbnail_job_completed_callback),
-					  ev_recent_view);
+							  G_CALLBACK (thumbnail_job_completed_callback),
+							  button);
 			ev_job_scheduler_push_job (EV_JOB (job), EV_JOB_PRIORITY_HIGH);
 			
-
-			/* The queue and the list own a ref to the job now */
 			g_object_unref (job);
 		}
 	}
@@ -187,14 +179,16 @@ ev_recent_view_refresh (EvRecentView *ev_recent_view)
 static void
 ev_recent_view_init (EvRecentView *ev_recent_view)
 {
-    EvRecentViewPrivate *priv;
+	EvRecentViewPrivate *priv;
+
     ev_recent_view->priv = G_TYPE_INSTANCE_GET_PRIVATE (ev_recent_view, EV_TYPE_RECENT_VIEW, EvRecentViewPrivate);
-    
+
     priv = ev_recent_view->priv;
-    priv->model = gtk_list_store_new (NUM_COLUMNS, 
-    								  GDK_TYPE_PIXBUF, 
-    								  G_TYPE_STRING,
-    								  G_TYPE_STRING);
+    gtk_widget_set_hexpand (GTK_WIDGET (ev_recent_view), TRUE);
+    gtk_widget_set_vexpand (GTK_WIDGET (ev_recent_view), TRUE);
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (ev_recent_view),
+                                    GTK_POLICY_NEVER,
+                                    GTK_POLICY_AUTOMATIC);
 }
 
 static void
@@ -204,62 +198,59 @@ ev_recent_view_constructed (GObject *object)
 	EvRecentViewPrivate *priv = ev_recent_view->priv;
 	
 	G_OBJECT_CLASS (ev_recent_view_parent_class)->constructed (object);
-	
-    priv->view = gtk_icon_view_new_with_model (GTK_TREE_MODEL (priv->model));
 
-    gtk_widget_set_hexpand (GTK_WIDGET (ev_recent_view), TRUE);
-    gtk_widget_set_vexpand (GTK_WIDGET (ev_recent_view), TRUE);
-    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (ev_recent_view),
-                                    GTK_POLICY_NEVER,
-                                    GTK_POLICY_AUTOMATIC);
+    priv->view = gtk_flow_box_new ();
                                     
-	gtk_icon_view_set_item_orientation (priv->view, GTK_ORIENTATION_HORIZONTAL);
-	gtk_icon_view_set_activate_on_single_click (priv->view, TRUE);
-	gtk_icon_view_set_pixbuf_column (priv->view, 0);
-	gtk_icon_view_set_text_column (priv->view, 1);
-	
-	g_signal_connect (priv->view, "item-activated",
-                          G_CALLBACK (on_icon_view_item_activated),
-                          ev_recent_view);
+	gtk_flow_box_set_activate_on_single_click (priv->view, TRUE);
+	gtk_flow_box_set_homogeneous (priv->view, FALSE);
 	
 	gtk_container_add (GTK_CONTAINER (ev_recent_view), priv->view);
 	gtk_widget_show (priv->view);
 	
+	priv->recent_manager = gtk_recent_manager_get_default ();
+	priv->recent_manager_changed_handler_id =
+            g_signal_connect_swapped (priv->recent_manager,
+                                      "changed",
+                                      G_CALLBACK (ev_recent_view_refresh),
+                                      ev_recent_view);
+                                      
 	ev_recent_view_refresh (ev_recent_view);
+	
 }
 
 static void
 ev_recent_view_dispose (GObject *obj)
 {
-        EvRecentView        *ev_recent_view = EV_RECENT_VIEW (obj);
-        EvRecentViewPrivate *priv = ev_recent_view->priv;
+	EvRecentView        *ev_recent_view = EV_RECENT_VIEW (obj);
+    EvRecentViewPrivate *priv = ev_recent_view->priv;
 
-        if (priv->model) {
-                ev_recent_view_clear_model (ev_recent_view);
-                g_object_unref (priv->model);
-                priv->model = NULL;
-        }
+	if (priv->recent_manager_changed_handler_id) {
+		g_signal_handler_disconnect (priv->recent_manager,
+		                             priv->recent_manager_changed_handler_id);
+		priv->recent_manager_changed_handler_id = 0;
+	}
+	priv->recent_manager = NULL;
 
-        G_OBJECT_CLASS (ev_recent_view_parent_class)->dispose (obj);
+    G_OBJECT_CLASS (ev_recent_view_parent_class)->dispose (obj);
 }
 
 static void
 ev_recent_view_class_init (EvRecentViewClass *klass)
 {
-        GObjectClass *g_object_class = G_OBJECT_CLASS (klass);
-        g_object_class->constructed = ev_recent_view_constructed;
-        g_object_class->dispose = ev_recent_view_dispose;
-        
-        signals[ITEM_ACTIVATED] =
-                  g_signal_new ("item-activated",
-                                EV_TYPE_RECENT_VIEW,
-                                G_SIGNAL_RUN_LAST,
-                                0, NULL, NULL,
-                                g_cclosure_marshal_generic,
-                                G_TYPE_NONE, 1,
-                                G_TYPE_STRING);
-                                
-        g_type_class_add_private (klass, sizeof (EvRecentViewPrivate));
+	GObjectClass *g_object_class = G_OBJECT_CLASS (klass);
+	g_object_class->constructed = ev_recent_view_constructed;
+	g_object_class->dispose = ev_recent_view_dispose;
+
+	signals[ITEM_ACTIVATED] =
+		      g_signal_new ("item-activated",
+		                    EV_TYPE_RECENT_VIEW,
+		                    G_SIGNAL_RUN_LAST,
+		                    0, NULL, NULL,
+		                    g_cclosure_marshal_generic,
+		                    G_TYPE_NONE, 1,
+		                    G_TYPE_STRING);
+		                    
+	g_type_class_add_private (klass, sizeof (EvRecentViewPrivate));
 }
 
 
