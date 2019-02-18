09c5c00dc9556083a8c6df34904d054bf3dd3702
raveit65 <mate@raveit.de>
2019-02-18 21:43:25 +0100

    ATRIL BACKPORTS: a11y: Do not use AtkFactory to create the EvViewAccessible
    Taken from
    https://github.com/mate-desktop/atril/commit/b461ca43a65f51dfd89bcb837ac6953a60a82d21
    
    Author: raveit65 <mate@raveit.de>
    Date:   Sun Mar 5 16:06:08 2017 +0100
    
        a11y: Do not use AtkFactory to create the EvViewAccessible
    
        https://bugzilla.gnome.org/show_bug.cgi?id=685828
    
        taken from:
        https://git.gnome.org/browse/evince/commit/?id=37c58de

--- libview/ev-view.h
+++ libview/ev-view.h
@@ -111,6 +111,8 @@ void               ev_view_focus_annotation          (EvView          *view,
 void               ev_view_begin_add_annotation      (EvView          *view,
                                                       EvAnnotationType annot_type);
 void               ev_view_cancel_add_annotation     (EvView          *view);
+void               ev_view_remove_annotation         (EvView          *view,
+                                                      EvAnnotation    *annot);
 
 /*For epub*/
 void               ev_view_disconnect_handlers       (EvView          *view);
