647e39b6a1fb1f9411865b2a0e4b9950fad9423b
Mickael <mickael.albertus@gmail.com>
2020-05-28 23:50:57 +0200

    xreader crash on epub search - reproducable
    BugFix#398

--- backend/epub/epub-document.c
+++ backend/epub/epub-document.c
@@ -181,7 +181,19 @@ epub_document_check_hits(EvDocumentFind *document_find,
 {
 	gchar *filepath = g_filename_from_uri((gchar*)page->backend_page,NULL,NULL);
 	htmlDocPtr htmldoc =  xmlParseFile(filepath);
+	
+	if (htmldoc == NULL) {
+		g_free (filepath);
+		return 0;
+	}
+		
 	htmlNodePtr htmltag = xmlDocGetRootElement(htmldoc);
+	if(htmltag == NULL) {
+		xmlFreeDoc(htmldoc);
+		g_free (filepath);
+		return 0;
+	}
+	
 	int count=0;
 	htmlNodePtr bodytag = htmltag->xmlChildrenNode;
 
