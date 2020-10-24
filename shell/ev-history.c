0137300c30b3de6caf326e19bcce79d50c2f442a
icarter09 <icarter1391@gmail.com>
2020-10-24 11:41:43 -0400

    Added keyboard shortcuts to history buttons

--- shell/ev-history.c
+++ shell/ev-history.c
@@ -492,7 +492,7 @@ page_changed_cb (EvDocumentModel *model,
                  gint             new_page,
                  EvHistory       *history)
 {
-	if (ABS (new_page - old_page) > 1)
+	if (ABS (new_page - old_page) >= 1)
 	{
 		ev_history_add_link_for_page (history, new_page);
 	}
