5fb80cd6db7797c771dccf05acfd34ac58873fdf
m8 <umusasadik@gmail.com>
2021-06-07 02:43:20 +0300

    Evince Backport:  Allow adding Highlight annotations from text selection

--- libview/ev-view.h
+++ libview/ev-view.h
@@ -112,6 +112,7 @@ void               ev_view_begin_add_annotation      (EvView          *view,
 void               ev_view_cancel_add_annotation     (EvView          *view);
 void               ev_view_remove_annotation         (EvView          *view,
                                                       EvAnnotation    *annot);
+gboolean       ev_view_add_text_markup_annotation_for_selected_text (EvView  *view);
 
 /*For epub*/
 void               ev_view_disconnect_handlers       (EvView          *view);
