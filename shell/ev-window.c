4362a3567245cde757d1fb2aca228470ccc8b392
Michael Webster <miketwebster@gmail.com>
2022-08-09 09:20:52 -0400

    ev-window.c: Don't try to handle keyboard events manually.
    This breaks keyboard shortcuts in epub/webview documents, and
    ends up triggering endless event recursion, wasting CPU and lagging
    the ui.
    
    Fixes #560.

--- shell/ev-window.c
+++ shell/ev-window.c
@@ -6050,14 +6050,6 @@ ev_window_key_press_event (GtkWidget   *widget,
         window->priv->menubar_show_queued = FALSE;
     }
 
-    /* Handle focus widget key events */
-    if (gtk_window_propagate_key_event (window, event))
-		return TRUE;
-
-	/* Handle mnemonics and accelerators */
-	if (gtk_window_activate_key (window, event))
-		return TRUE;
-
     /* Chain up, invokes binding set on window */
 	return GTK_WIDGET_CLASS (grand_parent_class)->key_press_event (widget, event);
 }
