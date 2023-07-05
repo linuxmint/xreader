65a3e5f647f70e578f47176ed1dc554a6638e127
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2023-07-05 13:20:54 +0200

    Add a setting to control window maximization on new documents

--- shell/ev-window.c
+++ shell/ev-window.c
@@ -1096,7 +1096,7 @@ ev_window_init_metadata_with_default_values (EvWindow *window)
         ev_metadata_set_boolean (metadata, "fullscreen", g_settings_get_boolean (settings, "fullscreen"));
     }
     if (!ev_metadata_has_key (metadata, "window_maximized")) {
-        ev_metadata_set_boolean (metadata, "window_maximized", TRUE);
+        ev_metadata_set_boolean (metadata, "window_maximized", g_settings_get_boolean (settings, "window-maximized"));
     }
 }
 
